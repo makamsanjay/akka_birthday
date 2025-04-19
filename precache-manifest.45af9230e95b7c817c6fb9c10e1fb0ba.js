45788a970d1b57f09a6c5da641c90e4e5eb3d1a9
sanjaymakam <151668960+sanjaymakam@users.noreply.github.com>
2025-04-19 14:46:29 -0400

    Updates

---
+++ precache-manifest.45af9230e95b7c817c6fb9c10e1fb0ba.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ef5afd0228c81ec9088ad498e76255",
+    "url": "/akka_birthday/index.html"
+  },
+  {
+    "revision": "dca8eef5b96921bc2b3b",
+    "url": "/akka_birthday/static/css/main.f000d725.chunk.css"
+  },
+  {
+    "revision": "47c2d691a9273271121f",
+    "url": "/akka_birthday/static/js/2.9c0fadf3.chunk.js"
+  },
+  {
+    "revision": "3201387ed07c0ee2e77a24fbc2161c48",
+    "url": "/akka_birthday/static/js/2.9c0fadf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad2391faf5acfcb54b5",
+    "url": "/akka_birthday/static/js/3.03e2e91b.chunk.js"
+  },
+  {
+    "revision": "dca8eef5b96921bc2b3b",
+    "url": "/akka_birthday/static/js/main.8219f5bd.chunk.js"
+  },
+  {
+    "revision": "d190cca838c79e241cad",
+    "url": "/akka_birthday/static/js/runtime-main.69de4640.js"
+  },
+  {
+    "revision": "8d8c6816684e617f9e41a660305ec2a8",
+    "url": "/akka_birthday/static/media/ballon.8d8c6816.png"
+  },
+  {
+    "revision": "bbd5faa035fdbb0a3cd020170af7df13",
+    "url": "/akka_birthday/static/media/coin.bbd5faa0.png"
+  },
+  {
+    "revision": "649b2cd2801146ecf69b0d81b62d911a",
+    "url": "/akka_birthday/static/media/dump1.649b2cd2.jpeg"
+  },
+  {
+    "revision": "52840f37dc7368f0eedb5e9fde373c3d",
+    "url": "/akka_birthday/static/media/dump10.52840f37.jpg"
+  },
+  {
+    "revision": "552a265ba37f370075d596f37cccf3c5",
+    "url": "/akka_birthday/static/media/dump11.552a265b.jpg"
+  },
+  {
+    "revision": "cc91d07165706b5a741c55bac71167c6",
+    "url": "/akka_birthday/static/media/dump12.cc91d071.jpg"
+  },
+  {
+    "revision": "224dd1ebd17c8b85f4f05e6b7debd814",
+    "url": "/akka_birthday/static/media/dump13.224dd1eb.jpg"
+  },
+  {
+    "revision": "506a8e52144685b0d36f858733b0b16e",
+    "url": "/akka_birthday/static/media/dump14.506a8e52.jpeg"
+  },
+  {
+    "revision": "f5c829f6e89db6dacbfc63c2c6c6d2b5",
+    "url": "/akka_birthday/static/media/dump15.f5c829f6.jpeg"
+  },
+  {
+    "revision": "08edadbc8a9e11d0bbdf45efd42c4d74",
+    "url": "/akka_birthday/static/media/dump18.08edadbc.mp4"
+  },
+  {
+    "revision": "edfe2f54358e2fc74405d1ffc24cc4c8",
+    "url": "/akka_birthday/static/media/dump19.edfe2f54.mp4"
+  },
+  {
+    "revision": "b761c79124352ea4cff945fa4e44c3b4",
+    "url": "/akka_birthday/static/media/dump2.b761c791.jpeg"
+  },
+  {
+    "revision": "4ffadd5b055a76aa8f10343460048280",
+    "url": "/akka_birthday/static/media/dump20.4ffadd5b.mp4"
+  },
+  {
+    "revision": "6c8edb2c48e23ba1a4ca610784ce2ca9",
+    "url": "/akka_birthday/static/media/dump21.6c8edb2c.mp4"
+  },
+  {
+    "revision": "4160e526de51af64a324c56c02febf03",
+    "url": "/akka_birthday/static/media/dump22.4160e526.mp4"
+  },
+  {
+    "revision": "964b61a4b0ba7df48634a9830c9495c0",
+    "url": "/akka_birthday/static/media/dump25.964b61a4.mp4"
+  },
+  {
+    "revision": "3b0ff1491bc04fdf9edba5758fb0fe0f",
+    "url": "/akka_birthday/static/media/dump26.3b0ff149.mp4"
+  },
+  {
+    "revision": "728c8990df7573b6132ade98920a5589",
+    "url": "/akka_birthday/static/media/dump29.728c8990.mp4"
+  },
+  {
+    "revision": "d2ad2b4bfe5718b70fcf63b330a3fa87",
+    "url": "/akka_birthday/static/media/dump3.d2ad2b4b.jpeg"
+  },
+  {
+    "revision": "233e86bb8874d70b8440d7b77d716015",
+    "url": "/akka_birthday/static/media/dump30.233e86bb.mp4"
+  },
+  {
+    "revision": "7757f0f1caa825e29c5440907fb700a0",
+    "url": "/akka_birthday/static/media/dump31.7757f0f1.mp4"
+  },
+  {
+    "revision": "194be945cc3881730c7f806f817e94a4",
+    "url": "/akka_birthday/static/media/dump33.194be945.mp4"
+  },
+  {
+    "revision": "2f39d4e87d2d8b9f68264d56ab0f2bca",
+    "url": "/akka_birthday/static/media/dump33.2f39d4e8.jpeg"
+  },
+  {
+    "revision": "c52da9115815865f1731a9fb8b4321fb",
+    "url": "/akka_birthday/static/media/dump34.c52da911.mp4"
+  },
+  {
+    "revision": "d35a3eccad8bb57f5f568e39b0c28051",
+    "url": "/akka_birthday/static/media/dump34.d35a3ecc.jpeg"
+  },
+  {
+    "revision": "0c65e64fb8fd6470f92a9a406918b165",
+    "url": "/akka_birthday/static/media/dump35.0c65e64f.mp4"
+  },
+  {
+    "revision": "2d8766e5eb31f352d8bc81e3fc76de86",
+    "url": "/akka_birthday/static/media/dump35.2d8766e5.jpeg"
+  },
+  {
+    "revision": "eca05a823f8edfea2ccaa590c3eb3a6d",
+    "url": "/akka_birthday/static/media/dump36.eca05a82.jpeg"
+  },
+  {
+    "revision": "563c752be8ee892de87bf3ee19fbf152",
+    "url": "/akka_birthday/static/media/dump37.563c752b.jpeg"
+  },
+  {
+    "revision": "9b1d1a9304dc92ff84a7e2d8ae17ea8a",
+    "url": "/akka_birthday/static/media/dump38.9b1d1a93.jpeg"
+  },
+  {
+    "revision": "08e048d1425d048a1229439179edf6fd",
+    "url": "/akka_birthday/static/media/dump4.08e048d1.jpeg"
+  },
+  {
+    "revision": "d444b16148239172d194ded0d870bdef",
+    "url": "/akka_birthday/static/media/dump44.d444b161.mov"
+  },
+  {
+    "revision": "07dbbc66b2547501e13dda7f2a2a6997",
+    "url": "/akka_birthday/static/media/dump5.07dbbc66.jpeg"
+  },
+  {
+    "revision": "7c4876ec1443139b911752f44d606199",
+    "url": "/akka_birthday/static/media/dump6.7c4876ec.jpeg"
+  },
+  {
+    "revision": "d12cffe0df00ad329686af657be1466d",
+    "url": "/akka_birthday/static/media/dump7.d12cffe0.jpeg"
+  },
+  {
+    "revision": "cbe8bcdf0e86f9ca5d346e990ea215a6",
+    "url": "/akka_birthday/static/media/dump8.cbe8bcdf.jpg"
+  },
+  {
+    "revision": "5adb35596efbb7feeadfc57682123c86",
+    "url": "/akka_birthday/static/media/dump9.5adb3559.jpg"
+  },
+  {
+    "revision": "c8fdd3074e61407f41fadb93ba6582ef",
+    "url": "/akka_birthday/static/media/giftbag.c8fdd307.png"
+  },
+  {
+    "revision": "37830dabaecb2ca3249dada66b8ac115",
+    "url": "/akka_birthday/static/media/giftbox.37830dab.png"
+  },
+  {
+    "revision": "426680f35e1b469f88eada91b66e44d9",
+    "url": "/akka_birthday/static/media/giftipad.426680f3.png"
+  },
+  {
+    "revision": "be3e6bbe19168768e26a5f947cf17676",
+    "url": "/akka_birthday/static/media/giftrev.be3e6bbe.png"
+  },
+  {
+    "revision": "c550e740dff4082ae92b92fd74defab1",
+    "url": "/akka_birthday/static/media/giftsparkle.c550e740.png"
+  },
+  {
+    "revision": "0307aded7641b2ec7341eba9482c0efe",
+    "url": "/akka_birthday/static/media/mainimage1.0307aded.jpeg"
+  },
+  {
+    "revision": "52c268ce65156c775525c042d6c515e6",
+    "url": "/akka_birthday/static/media/mainimage10.52c268ce.jpeg"
+  },
+  {
+    "revision": "f7d169e5d614e9ec23e15d372ab1b7ee",
+    "url": "/akka_birthday/static/media/mainimage2.f7d169e5.png"
+  },
+  {
+    "revision": "4ad80cba4db11b48139e097c56410844",
+    "url": "/akka_birthday/static/media/mainimage3.4ad80cba.jpeg"
+  },
+  {
+    "revision": "eca825a09e13aa49ae11f629d77c11ca",
+    "url": "/akka_birthday/static/media/mainimage4.eca825a0.jpeg"
+  },
+  {
+    "revision": "99876a1605953eec0b79d1e31653e366",
+    "url": "/akka_birthday/static/media/mainimage5.99876a16.jpeg"
+  },
+  {
+    "revision": "76bcb01ef8eb7f2adf7b872886be31b5",
+    "url": "/akka_birthday/static/media/mainimage6.76bcb01e.jpeg"
+  },
+  {
+    "revision": "cd4700df0fa79b17e21f1e369d4d2ca0",
+    "url": "/akka_birthday/static/media/mainimage7.cd4700df.jpg"
+  },
+  {
+    "revision": "3ef648fb41cc32eedc14297d24acf91f",
+    "url": "/akka_birthday/static/media/mainimage8.3ef648fb.jpg"
+  },
+  {
+    "revision": "af2305e10f1346c317547681fc452ae6",
+    "url": "/akka_birthday/static/media/mainimage9.af2305e1.jpg"
+  },
+  {
+    "revision": "52840f37dc7368f0eedb5e9fde373c3d",
+    "url": "/akka_birthday/static/media/photoGallery1.52840f37.jpg"
+  },
+  {
+    "revision": "5adb35596efbb7feeadfc57682123c86",
+    "url": "/akka_birthday/static/media/photoGallery2.5adb3559.jpg"
+  },
+  {
+    "revision": "fa2b351d047554f6badaf90f2653b39d",
+    "url": "/akka_birthday/static/media/photoGallery3.fa2b351d.jpg"
+  },
+  {
+    "revision": "aab92dfaab3bf8035935b545e61bdaf8",
+    "url": "/akka_birthday/static/media/photogallery4.aab92dfa.jpg"
+  },
+  {
+    "revision": "a68cec1febe56fcdf82fad41255fe3e2",
+    "url": "/akka_birthday/static/media/recreate1.a68cec1f.jpeg"
+  },
+  {
+    "revision": "b626f59a6707ae3d581e11d38db19fc0",
+    "url": "/akka_birthday/static/media/recreate2.b626f59a.jpeg"
+  },
+  {
+    "revision": "0d7b751f0edb0e9000a4fd57b199b836",
+    "url": "/akka_birthday/static/media/recreate3.0d7b751f.jpg"
+  },
+  {
+    "revision": "cbe76ef9890875065ea7ea22d7f2ca72",
+    "url": "/akka_birthday/static/media/recreate4.cbe76ef9.jpg"
+  },
+  {
+    "revision": "52e9632528d462f3c4de372159c6c391",
+    "url": "/akka_birthday/static/media/recreate5.52e96325.jpg"
+  },
+  {
+    "revision": "5212ef4b50a58dd2808d089ce56b7c70",
+    "url": "/akka_birthday/static/media/test.5212ef4b.png"
+  },
+  {
+    "revision": "079eb15328daaaa07e7f48e29941e8a7",
+    "url": "/akka_birthday/static/media/videonote1.079eb153.mp4"
+  },
+  {
+    "revision": "68eeb60bc093b76ad1f7e2034756035a",
+    "url": "/akka_birthday/static/media/videonote2.68eeb60b.mp4"
+  },
+  {
+    "revision": "1e20a55a8beb80f790768a3783cdc129",
+    "url": "/akka_birthday/static/media/videonote3.1e20a55a.mp4"
+  },
+  {
+    "revision": "c808bdb463128a0b6a02c4bceefccc55",
+    "url": "/akka_birthday/static/media/videonote4.c808bdb4.mp4"
+  },
+  {
+    "revision": "5eb453f84c0e1d9e1212afaecf1eb7e8",
+    "url": "/akka_birthday/static/media/videonote5.5eb453f8.mp4"
+  }
+]);
